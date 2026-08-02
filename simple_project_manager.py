16b67379a6a51c3d8e5f4a83dec72a57e9d9961d
JDE-Projects <289071709+JDE-Projects@users.noreply.github.com>
2026-08-01 21:06:10 -0500

    Bump version to v1.3.2

--- simple_project_manager.py
+++ simple_project_manager.py
@@ -24,7 +24,7 @@ from openpyxl.styles import Font, PatternFill, Alignment
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.datavalidation import DataValidation
 
-APP_VERSION = "1.3.1"
+APP_VERSION = "1.3.2"
 GITHUB_OWNER = "JDE-Projects"
 GITHUB_REPO = "Simple-Project-Manager"
 
